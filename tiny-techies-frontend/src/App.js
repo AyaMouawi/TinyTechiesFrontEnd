57ca32e1946fefe41bf344a44cfb04c1ace7db3c
AyaMouawi <ayamouawi8@gmail.com>
2023-10-13 12:05:16 +0300

    Restart

--- tiny-techies-frontend/src/App.js
+++ tiny-techies-frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import HomePage from "./Components/AboutUs"
+
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+    <p>Hello</p>
     </div>
   );
 }
