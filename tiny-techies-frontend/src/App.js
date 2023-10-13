a273432d7c49bc3f0b0c901b6cf7baa567f93472
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-10-13 10:10:22 +0300

    Statistics

--- tiny-techies-frontend/src/App.js
+++ tiny-techies-frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from "./Components/AboutUs"
+import HomePage from "./Components/HomePage"
 
 
 function App() {
--- tiny-techies-frontend/src/Components/Statistics.jsx
+++ tiny-techies-frontend/src/Components/Statistics.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import counter from "../images/counter-bg.jpg";
-import book from "../images/book-open-reader-solid.svg";
-import person from "../images/person-chalkboard-solid.svg";
-import graduate from "../images/user-graduate-solid.svg";
-import laptop from "../images/laptop-code-solid.svg";
+
 import '../css/statics.css';
 
 function Statistics() {
   return (
     <div className="statics-container">
-      <div src={counter} className="counter-image">
+      <div src="/Images/person-chalkboard-solid.svg" className="counter-image">
         <div className="sub-stat-container">
           <div>
-            <section className="image-section"> <img src={person} className="icon_stat" alt="book icon" /></section>
+            <section className="image-section"> <img src="/Images/person-chalkboard-solid.svg" className="icon_stat" alt="book icon" /></section>
            
             <p>230</p>
             <button>Trainer</button>
           </div>
           <div>
-          <section className="image-section"> <img src={graduate} className="icon_stat" alt="book icon" /></section>
+          <section className="image-section"> <img src="/Images/user-graduate-solid.svg" className="icon_stat" alt="book icon" /></section>
             <p>230</p>
             <button>Student</button>
           </div>
@@ -26,12 +22,12 @@ function Statistics() {
 
         <div className="sub-stat-container">
           <div>
-          <section className="image-section"> <img src={book} className="icon_stat" alt="book icon" /></section>
+          <section className="image-section"> <img src="/Images/book-open-reader-solid.svg" className="icon_stat" alt="book icon" /></section>
             <p>230</p>
             <button>Courses</button>
           </div>
           <div>
-          <section className="image-section"> <img src={laptop} className="icon_stat" alt="book icon" /></section>
+          <section className="image-section"> <img src="/Images/laptop-code-solid.svg" className="icon_stat" alt="book icon" /></section>
             <p>230</p>
             <button>Projects</button>
           </div>
