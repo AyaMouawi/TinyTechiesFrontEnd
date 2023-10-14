122ffec07d0975a3889c965b8bbf7612dd690691
AyaMouawi <ayamouawi8@gmail.com>
2023-10-14 20:27:18 +0300

    Emptying The app.js

--- tiny-techies-frontend/src/App.js
+++ tiny-techies-frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import HeroSection from './Components/HeroSection';
-import NavBar from './Components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-    <NavBar/> 
-    <HeroSection/>
+    
     </div>
   );
 }
