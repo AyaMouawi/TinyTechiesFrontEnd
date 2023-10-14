4defe75a22fcc6a4b48eee6941e67fa3018111e7
AyaMouawi <ayamouawi8@gmail.com>
2023-10-14 21:01:27 +0300

    Preparing For the merge

--- tiny-techies-frontend/src/App.js
+++ tiny-techies-frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import CoursesSection from './Components/CoursesSection';
+
 
 function App() {
   return (
     <div className="App">
-    <CoursesSection/>
+ 
     </div>
   );
 }
