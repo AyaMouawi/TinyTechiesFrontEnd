15972abec1da4648a132caafd5996a75c724164f
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-10-16 18:41:31 +0300

    fghj

---
+++ tiny-techies-frontend/src/Components/SideBar.jsx
@@ -0,0 +1,25 @@
+
+import '../css/SideBar.css';
+import TrainerDashboeard from './TrainerDashboardWelcome'
+function SideBar() {
+  return (
+    <div className='SideBarContainer'>
+    <div className='SideBarSubContainer' >
+    <img src="../../Images/TinyTechiesDashLogo.png" className='TinyTechiesImg'></img>
+    <p>Welcome</p>
+    <p>Trainer Name</p>
+    <hr></hr>
+<button className='SideBarButton'>Assignments</button>
+<button className='SideBarButton'>Attendance</button>
+<button className='SideBarButton'>Courses</button>   
+</div>
+   <div>
+   <img src="../../Images/Children.png" className='ChildrenImg'></img>
+   <TrainerDashboeard/>
+   </div>
+   
+   </div>
+  );
+}
+
+export default SideBar;
