d0d1ee71c74bacd54691e80ec34a3d93d44ee671
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-10-24 19:47:25 +0300

    Add Assignment Fetching  Done

--- tiny-techies-frontend/src/TrainerDashpages/TrainerDashAssignment.jsx
+++ tiny-techies-frontend/src/TrainerDashpages/TrainerDashAssignment.jsx
@@ -1,25 +1,75 @@
 import React, { useState } from "react";
-// css
 import "../TrainerDashCSS/TrainerDashAssignment.css";
+import axios from "axios";
+
 const TrainerDashAssignment = () => {
   const [selectedCourse, setSelectedCourse] = useState("");
+  const [zoomLink, setZoomLink] = useState("");
+  const [assignmentName, setAssignmentName] = useState("");
+  const [meetingDate, setMeetingDate] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [assignmentFile, setAssignmentFile] = useState(null);
+  const [assignmentRequirement, setAssignmentRequirement] = useState("");
 
   const courses = ["Course 1", "Course 2", "Course 3", "Course 4"];
+
   const handleCourseChange = (event) => {
     setSelectedCourse(event.target.value);
   };
 
+  // Function to handle adding an assignment
+  const handleAddAssignment = async () => {
+    // Check if the required fields are not empty
+    if (
+      selectedCourse === "" ||
+      zoomLink === "" ||
+      assignmentName === "" ||
+      meetingDate === "" ||
+      dueDate === "" ||
+      !assignmentFile ||
+      assignmentRequirement === ""
+    ) {
+      console.error("Please fill in all the required fields.");
+      return;
+    }
+
+    const assignmentData = new FormData();
+    assignmentData.append("AssignmentDueDate", dueDate);
+    assignmentData.append("ZoomLink", zoomLink);
+    assignmentData.append("MeetingDate", meetingDate);
+    assignmentData.append("AssignmentRequirement", assignmentRequirement);
+    assignmentData.append("AssignmentName", assignmentName);
+    assignmentData.append("CourseName", selectedCourse); // Sending course name
+
+    // Append the selected file to the form data
+    assignmentData.append("file", assignmentFile);
+
+    try {
+      // Send a POST request to add the assignment
+      const response = await axios.post("http://localhost:8000/assignmentContent/add", assignmentData);
+
+      if (response.status === 201) {
+        console.log("Data added successfully");
+        // Optionally, reset the form or perform any other action upon successful data addition
+      } else {
+        console.error("Error adding data.");
+      }
+    } catch (error) {
+      console.error("Error during data addition:", error);
+    }
+  };
+
   return (
-    <div className='dash-assignment'>
-      <h2 className='assignment-title'>Add Assignment</h2>
-      <div className='assignment-input-list'>
-        <div className='assignment-input'>
+    <div className="dash-assignment">
+      <h2 className="assignment-title">Add Assignment</h2>
+      <div className="assignment-input-list">
+        <div className="assignment-input">
           <select
             value={selectedCourse}
             onChange={handleCourseChange}
-            className='assignment-select'
+            className="assignment-select"
           >
-            <option value=''>Your Course</option>
+            <option value="">Your Course</option>
             {courses.map((course, index) => (
               <option key={index} value={course}>
                 {course}
@@ -27,40 +77,69 @@ const TrainerDashAssignment = () => {
             ))}
           </select>
         </div>
-        <div className='assignment-input'>
-        
-          <input type='text' name='' id='' className='assignment-zoom' placeholder="Zoom Link" />
+        <div className="assignment-input">
+          <label htmlFor="assignment-zoom">Zoom Link</label>
+          <input
+            type="text"
+            id="assignment-zoom"
+            placeholder="Zoom Link"
+            value={zoomLink}
+            onChange={(e) => setZoomLink(e.target.value)}
+          />
         </div>
-        <div className='assignment-input'>
-        
-        <input type='text' name='' id='' className='assignment-zoom' placeholder="Assignment Name" />
-      </div>
-        <div className='assignment-input-meeting'>
-        <div className='TrainerDashAssignmentDates'>
-            <label htmlFor=''>Meeting Date</label>
-            <input type='date' name='' id='' className='meeting-date' />
-            </div>
-            <div className='TrainerDashAssignmentDates'>
-            <label htmlFor=''>Due Date</label>
-            <input type='date' name='' id='' className='meeting-date' />
-            </div>
+        <div className="assignment-input">
+          <label htmlFor="assignment-name">Assignment Name</label>
+          <input
+            type="text"
+            id="assignment-name"
+            placeholder="Assignment Name"
+            value={assignmentName}
+            onChange={(e) => setAssignmentName(e.target.value)}
+          />
+        </div>
+        <div className="assignment-input-meeting">
+          <div className="TrainerDashAssignmentDates">
+            <label htmlFor="meeting-date">Meeting Date</label>
+            <input
+              type="date"
+              id="meeting-date"
+              value={meetingDate}
+              onChange={(e) => setMeetingDate(e.target.value)}
+            />
           </div>
-         
-        
-        
-          <div className='TrainerDashAssignmentFile'>
-            <label htmlFor=''>Assignment File</label>
-          
-            <input type='file' name='' id='' className="" />
+          <div className="TrainerDashAssignmentDates">
+            <label htmlFor="due-date">Due Date</label>
+            <input
+              type="date"
+              id="due-date"
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+            />
           </div>
-          
-       
-        <div className='assignment-input'>
-          <label htmlFor=''>Assignment Requirements </label>
-          <textarea name='' id='' className='assignment-requirement'></textarea>
+        </div>
+        <div className="TrainerDashAssignmentFile">
+          <label htmlFor="assignment-file">Assignment File</label>
+          <input
+            type="file"
+            id="assignment-file"
+            onChange={(e) => setAssignmentFile(e.target.files[0])}
+          />
+        </div>
+        <div className="assignment-input">
+          <label htmlFor="assignment-requirement">Assignment Requirements</label>
+          <textarea
+            id="assignment-requirement"
+            value={assignmentRequirement}
+            onChange={(e) => setAssignmentRequirement(e.target.value)}
+          ></textarea>
         </div>
       </div>
-      <button type='submit' className='assignment-btn-submit'>
+
+      <button
+        type="button"
+        className="assignment-btn-submit"
+        onClick={handleAddAssignment}
+      >
         Submit
       </button>
     </div>
