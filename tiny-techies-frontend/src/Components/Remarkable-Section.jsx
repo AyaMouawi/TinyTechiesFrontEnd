b52418d03f8f464bf9f6cf9d575b356561a6111a
AyaMouawi <ayamouawi8@gmail.com>
2023-11-02 09:10:18 +0200

    FixingRemarkable

---
+++ tiny-techies-frontend/src/Components/Remarkable-Section.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import RemarkableProject from './RemarkableProject';
+
+const RemarkableSection = () => {
+  const [projects, setProjects] = useState([]);
+
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL);
+    axios.get(`${process.env.REACT_APP_API_URL}/projects/getRemarkableProjects`)
+      .then((response) => {
+        setProjects(response.data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div className='remarkable-wrapper'>
+      <div className='remarkable-title'>
+        <h1>Remarkable Projects</h1>
+      </div>
+      <div className='remarkable-container'>
+        {projects.map((project) => (
+          <RemarkableProject
+            key={project.Project_id}
+            StudentName={project.UserName}
+            ProjectName={project.ProjectName}
+            ProjectDesc={project.ProjectDescription}
+            ProjectUrl={project.ProjectFile}
+            StudentImage={project.StudentImage}
+            StudentProject={project.StudentProject}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RemarkableSection;
